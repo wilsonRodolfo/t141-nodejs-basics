c17143c34aba593b8f9b127be82153a3484e9f23
Wilson Rodolfo <rodolfo.souza@treetech.com.br>
2017-07-01 16:07:04 -0300

    Added routes to example

--- 06-express/app/index.js
+++ 06-express/app/index.js
@@ -1,3 +1,22 @@
 const {host, port} = require('./config.js')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-console.log(host, port)
+//setup
+const app = express()
+app.use(bodyParser.json())
+//app.use(express.static('public'))
+
+//publish HTML5 app
+app.get('/', (request, response) => {
+    response.send("Hi Wilson")
+})
+
+app.get('/about', (request, response) => {
+    response.send('This is an express application')
+})
+
+//server startup
+app.listen(port, () => {
+    console.log(`Server statarted at http://${host}:${port}`)
+})
